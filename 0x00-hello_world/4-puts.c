142ddd3a923fbded0a02ba76ee679cc805c0ccf2
tenderheartviv <vivian.emenike@yahoo.com>
2023-02-16 16:32:08 -0800

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-/*
- * main	- print a line of code using puts
- *
- * Return: 0
-*/
+
+/**
+ *  * main - Entry point
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *    * Return: 0
+ *     */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");		     return (0);
-}	
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- * main - print a c program using printf
- * 
- * Return: 0
+ *   main - Print a string
+ *   Description: use printf
+ *   Return: 0
 */
+
 int main(void)
-{	
-	printf("with proper grammar, but the outcome is a piece of art, \n");		return (0)
-}	
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
