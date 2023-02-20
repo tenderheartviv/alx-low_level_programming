5806959055e9ad8b0960a4cf18e72e8914dc609b
tenderheartviv <vivian.emenike@yahoo.com>
2023-02-20 11:59:40 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,33 @@
-nclude <stdio.h>
-#include <stdlib.h>
-
+#include <stdio.h>
 /**
- *  main - returns a combination of number
- *  Return: return 0 if successful
- */
-
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-			int i, j;
-						
-						for (i = 0; i < 100; i++)
-												{
-																					for (j = 0; j < 100; j++)
-																																	{
-																																																	if (i < j)
-																																																																					{
-																																																																																														putchar((i / 10) + 48);
-																																																																																																																										putchar((i % 10) + 48);
-																																																																																																																																																										putchar(' ');
-																																																																																																																																																																																														putchar((j / 10) + 48);
-																																																																																																																																																																																																																																						putchar((j % 10) + 48);
-																																																																																																																																																																																																																																																																																		if (i != 98 || j != 99)
-																																																																																																																																																																																																																																																																																																																																				{
-																																																																																																																																																																																																																																																																																																																																																																																												putchar(',');
-																																																																																																																																																																																																																																																																																																																																																																																																																																																								putchar(' ');
-																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								}
-																																																																																																																																																																																																																																																																																																																																	}
-																																																																		}
-																														}
-										
-										putchar('\n');
+		int i, j;
 
-															return (0);
+			for (i = 0; i <= 98; i++)
+					{
+								for (j = i + 1; j <= 99; j++)
+											{
+															putchar(i / 10 + '0');
+																		putchar(i % 10 + '0');
+																					putchar(' ');
+																								putchar(j / 10 + '0');
+																											putchar(j % 10 + '0');
+
+																														if (i == 98 && j == 99)
+																																		{
+																																							continue;
+																																										}
+
+																																	putchar(',');
+																																				putchar(' ');
+																																						}
+									}
+
+				putchar('\n');
+
+					return (0);
 }
